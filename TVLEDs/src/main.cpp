a408d3c630010a4b3a19f80c60751a80aaf4a262
Hayes Barber <barberhayes@gmail.com>
2025-07-11 20:00:32 -0400

    add strip driver

--- TVLEDs/src/main.cpp
+++ TVLEDs/src/main.cpp
@@ -1,15 +1,24 @@
 #include <Arduino.h>
 #include <AutoWiFi.h>
+#include <LEDStripDriver.h>
+
+const uint16_t NUM_PIXELS = 200;
+const uint16_t BRIGHTNESS = 42;
+const uint16_t DATA_PIN = 13;
 
 AutoWiFi wifi;
+LEDStripDriver stripDriver;
 
 void setup() {
   Serial.begin(115200);
   while(!Serial);
 
   wifi.connect();
+
+  stripDriver.init<DATA_PIN>(NUM_PIXELS, BRIGHTNESS);
 }
 
 void loop() {
   wifi.loop();
+  stripDriver.loop();
 }
