bf9952ef9cd4b018d67b9b4f2f90729b462b093b
Hayes Barber <barberhayes@gmail.com>
2025-08-16 18:50:45 -0400

    auto commit

--- Dial/src/main.cpp
+++ Dial/src/main.cpp
@@ -1,1 +1,30 @@
 #include <Arduino.h>
+#include <AutoWiFi.h>
+#include <GFXDriver.h>
+
+AutoWiFi wifi;
+GFXDriver screen;
+
+void onScreenTouch() {}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+
+  AutoWiFi::State state = wifi.connect();
+
+  if (state == AutoWiFi::State::AP_MODE) {
+    return;
+  }
+
+  screen.init(onScreenTouch);
+}
+
+void loop() {
+  wifi.loop();
+  if (wifi.getState() == AutoWiFi::State::AP_MODE)
+    return;
+
+  screen.loop();
+}
