47f1155c2520e69086f34a55517f5c7f5ee8ec53
Hayes Barber <barberhayes@gmail.com>
2025-08-16 19:01:10 -0400

    add button

--- Dial/src/main.cpp
+++ Dial/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <AutoWiFi.h>
+#include <ButtonHandler.h>
 #include <GFXDriver.h>
 #include <RestBeacon.h>
 #include <RotaryEvents.h>
@@ -8,10 +9,12 @@ const uint16_t HTTP_PORT = 80;
 const uint16_t UDP_PORT = 4210;
 const uint8_t ENCODER_CLK = 13;
 const uint8_t ENCODER_DT = 10;
+const uint8_t BUTTON_PIN = 14;
 
 AutoWiFi wifi;
 GFXDriver screen;
 RestBeacon beacon(HTTP_PORT, UDP_PORT);
+Button button;
 
 void udpTask(void *pvParameters) {
   while (1) {
@@ -29,6 +32,8 @@ void onDiscovery(IPAddress sender, uint16_t port, const String &message) {}
 void onLeftTurn() {}
 
 void onRightTurn() {}
+
+void onButtonPressed(int pin) {}
 
 void setup() {
   Serial.begin(115200);
@@ -50,6 +55,8 @@ void setup() {
   RotaryEvents::getInstance().init(ENCODER_CLK, ENCODER_DT, onLeftTurn,
                                    onRightTurn, 2);
 
+  button.init(BUTTON_PIN, onButtonPressed);
+
   xTaskCreatePinnedToCore(udpTask, "UdpTask", 4096, nullptr, 2, nullptr, 1);
 }
 
@@ -60,4 +67,5 @@ void loop() {
 
   screen.loop();
   beacon.loopHttp();
+  button.update();
 }
