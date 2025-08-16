87adadcdecc558d8c74b6c148a4f668866584c8d
Hayes Barber <barberhayes@gmail.com>
2025-08-16 18:54:26 -0400

    add beacon

--- Dial/src/main.cpp
+++ Dial/src/main.cpp
@@ -1,11 +1,27 @@
 #include <Arduino.h>
 #include <AutoWiFi.h>
 #include <GFXDriver.h>
+#include <RestBeacon.h>
+
+const uint16_t HTTP_PORT = 80;
+const uint16_t UDP_PORT = 4210;
 
 AutoWiFi wifi;
 GFXDriver screen;
+RestBeacon beacon(HTTP_PORT, UDP_PORT);
+
+void udpTask(void *pvParameters) {
+  while (1) {
+    beacon.loopUdp();
+    vTaskDelay(pdMS_TO_TICKS(50));
+  }
+}
 
 void onScreenTouch() {}
+
+String onMessage(const Message &msg) {}
+
+void onDiscovery(IPAddress sender, uint16_t port, const String &message) {}
 
 void setup() {
   Serial.begin(115200);
@@ -19,6 +35,12 @@ void setup() {
   }
 
   screen.init(onScreenTouch);
+
+  beacon.onMessage(onMessage);
+  beacon.onDiscovery(onDiscovery);
+  beacon.begin();
+
+  xTaskCreatePinnedToCore(udpTask, "UdpTask", 4096, nullptr, 2, nullptr, 1);
 }
 
 void loop() {
@@ -27,4 +49,5 @@ void loop() {
     return;
 
   screen.loop();
+  beacon.loopHttp();
 }
