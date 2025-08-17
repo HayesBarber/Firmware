34fb54aeb682e43e5573f6d715eb0a10361407bd
Hayes Barber <barberhayes@gmail.com>
2025-08-17 13:04:29 -0400

    add models

--- Dial/src/models.h
+++ Dial/src/models.h
@@ -21,3 +21,14 @@ struct DisplayData {
   String time;
   std::vector<String> extras;
 };
+
+struct AppState {
+  std::vector<Device> devices;
+  std::vector<Theme> themes;
+  DisplayData displayData;
+  unsigned long lastActivityDetected;
+  bool displayIsOn;
+  int rotationIndex;
+};
+
+enum class UIState { Idle, ShowingDevices, ShowingThemes };
